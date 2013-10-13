704dc45839f49f21d3ae8222e4fe42035c009866
Rihard Wålander <richard.walander@telepo.com>
2013-10-13 17:58:57 +0200

    Refactored background to use require.js

--- src/js/background.js
+++ src/js/background.js
@@ -1,31 +1,19 @@
-chrome.runtime.onMessage.addListener(
-function(request, sender, sendResponse) {
-	// console.log(sender.tab ?
-	// 	"from a content script:" + sender.tab.url :
-	// 	"from the extension");
-	// if (request.greeting == "hello")
-	// 	sendResponse({farewell: "goodbye"});
-	console.log(request, sender);
-	if(request.crud == 'read') {
-		// console.log(chrome);
-		chrome.storage.sync.get(sender.tab.url, function (models) {
-			console.log('read', models);
-			sendResponse({models: models[sender.tab.url]});
-		});
-	} else if (request.crud == 'create') {
-		console.log('create', request.models);
-		var obj = {};
-		obj[sender.tab.url] = request.models;
-		chrome.storage.sync.set(obj, function () {
-			sendResponse({msg: 'saved'});
-		});
-	} else if (request.crud == 'delete') {
-		console.log('delete', request.models);
-		var obj = {};
-		obj[sender.tab.url] = request.models;
-		chrome.storage.sync.set(obj, function () {
-			sendResponse({msg: 'saved'});
-		});
+require.config({
+	"baseUrl": "js/",
+	"paths": {
+		"jquery": "lib/jquery",
+		"backbone": "lib/backbone",
+		"underscore": "lib/underscore"
+	},
+	shim: {
+		'underscore': {
+			exports: '_'
+		},
+		'backbone': {
+			deps: ['underscore', 'jquery'],
+			exports: 'Backbone'
+		}
 	}
-	return true;
 });
+
+require(['background/App']);
---
+++ src/js/background/App.js
@@ -0,0 +1,43 @@
+"use strict";
+ 
+/**
+ * App
+ * The App module.
+ * @author Richard Wålander
+ */
+define([
+	"jquery"
+], function($) {
+	chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		// console.log(sender.tab ?
+		// 	"from a content script:" + sender.tab.url :
+		// 	"from the extension");
+		// if (request.greeting == "hello")
+		// 	sendResponse({farewell: "goodbye"});
+		console.log(request, sender);
+		if(request.crud == 'read') {
+			// console.log(chrome);
+			chrome.storage.sync.get(sender.tab.url, function (models) {
+				console.log('read', models);
+				sendResponse({models: models[sender.tab.url]});
+			});
+		} else if (request.crud == 'create') {
+			console.log('create', request.models);
+			var obj = {};
+			obj[sender.tab.url] = request.models;
+			chrome.storage.sync.set(obj, function () {
+				sendResponse({msg: 'saved'});
+			});
+		} else if (request.crud == 'delete') {
+			console.log('delete', request.models);
+			var obj = {};
+			obj[sender.tab.url] = request.models;
+			chrome.storage.sync.set(obj, function () {
+				sendResponse({msg: 'saved'});
+			});
+		}
+		return true;
+	});
+	return {};
+});
