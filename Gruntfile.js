8ae6be024cac881aa414e9e23bfbc6aaf5e12d57
Rihard Wålander <richard.walander@telepo.com>
2013-11-11 23:15:44 +0100

    Added grunt and grunt release

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    release: {
+        options: {
+        file: 'manifest.json'//, //default: package.json
+        // github: { 
+        //   repo: 'geddski/grunt-release', //put your user/repo here
+        //   usernameVar: 'GITHUB_USERNAME', //ENVIRONMENT VARIABLE that contains Github username 
+        //   passwordVar: 'GITHUB_PASSWORD' //ENVIRONMENT VARIABLE that contains Github password
+        // }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-release');
+
+  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+
+};
